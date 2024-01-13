24a9cb40c5f500c44e81f6ba1144ecd820fb646e
mohammedasif9786 <asif.jdp@gmail.com>
2024-01-13 16:11:40 +0530

    finding total layers in array and counting all duplicate layers as single layer

--- totalLayers.js
+++ totalLayers.js
@@ -2,8 +2,20 @@ let totalLayersInarr =(input)=>{
 
     let layersCount =0
 
-    
+    let dupArr = input.slice()
 
+    for(let i=0 ; i<dupArr.length; i++){
+        for(let j=i+1 ; j<input.length ;j++){
+            if(input[i] == input[j]){
+                dupArr.splice([i],1)
+            }
+        }
     }
+    let totalLength =0
+    return totalLength = dupArr.length
+
+}
+
+
 
 
